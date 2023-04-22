f855f4edfd3ffd03a070157244648a3f428843cf
RomaRina22 <s22romarina@venta.lv>
2023-04-22 09:38:42 +0300

    mystack isfull()

--- src/model/MyStack.java
+++ src/model/MyStack.java
@@ -3,6 +3,7 @@ package model;
 public class MyStack<T> {
 	private MyNodeS<T> topNode;
 	private int length;
+	private final int MAX_SIZE = 100000;
 	
 	public MyStack() {
 		super();
@@ -10,5 +11,15 @@ public class MyStack<T> {
 		length = 0;
 	}
 	
+	public boolean isFull() { //nav īsti jēgas no šī...
+		/*if (length < MAX_SIZE) {
+			return false;
+		}
+		else {
+			return true;
+		}*/
+		final int MEMORY_TRESHOLD = 10;
+		return (Runtime.getRuntime().freeMemory() > MEMORY_TRESHOLD);
+	}
 	
 }
