8d408501718d8baa1d299a2df6695da349ddb366
RomaRina22 <s22romarina@venta.lv>
2023-04-22 10:17:56 +0300

    myqueue isfull()

--- src/model/MyQueue.java
+++ src/model/MyQueue.java
@@ -4,10 +4,15 @@ public class MyQueue<T> {
 	private MyNodeQ<T> frontNode;
 	private MyNodeQ<T> rearNode;
 	private int length;
+	private final int MEMORY_TRESHOLD = 10; //for isfull
 	
 	public MyQueue() {
 		frontNode = null;
 		rearNode = null;
 		length = 0;
+	}
+	
+	public boolean isFull() {
+		return (Runtime.getRuntime().freeMemory() > MEMORY_TRESHOLD);
 	}
 }
