853a68366438087bc893b73900e9bf98367bde62
RomaRina22 <s22romarina@venta.lv>
2023-04-22 12:38:30 +0300

    added syntaxcheck fucntion

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,39 +1,63 @@
 package service;
 
+import java.io.File;
+import java.time.temporal.UnsupportedTemporalTypeException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import model.MyQueue;
+import model.MyStack;
+import model.Student;
 
 public class MainService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Runtime.getRuntime().freeMemory());
-		MyQueue<Integer> que= new MyQueue<>();
-		System.out.println(Runtime.getRuntime().freeMemory());
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		que.enqueue(5);
-		System.out.println(Runtime.getRuntime().freeMemory());
-		que.print();
-		System.out.println(Runtime.getRuntime().freeMemory());
-		Scanner myscan = new Scanner(System.in);
-		while (true) {
-			int newint = myscan.nextInt();
-			if (newint == 0) break;
-			else {
-				que.enqueue(newint);
-				que.print();
-				System.out.println(Runtime.getRuntime().freeMemory());
-			}
-			
-		}
-		myscan.close();
+		MyStack<Integer> intStack = new MyStack<>();
+		MyStack<Student> stuStack = new MyStack<>();
+		
 	}
-
+	
+	public boolean syntaxCheck (File inFile) {
+		int line = 0;
+		boolean fileOk = true;
+		try {
+			Scanner fileScan = new Scanner(inFile);
+			MyStack<Character> charStack = new MyStack<>();
+			while (fileScan.hasNext()) {
+				String newLine = fileScan.nextLine();
+				line++;
+				for (char temp: newLine.toCharArray()) {
+					if (temp == '[' || temp == '(' || temp == '{') {
+						charStack.push(temp);
+					}
+					else {
+						if (temp == ']' || temp == ')' || temp == '}') {
+							char expectedChar = ' ';
+							char lastOpenBrace = charStack.pop(); 
+							switch(lastOpenBrace) {
+							case '[':
+								expectedChar = ']';
+								break;
+							case '{':
+								expectedChar = '}';
+								break;
+							case '(':
+								expectedChar = ')';
+								break;
+							}
+							if (expectedChar != temp) {
+								fileOk = false;
+								System.out.println("Found error at "+inFile.getName()+"("+line+")"+", expected "+expectedChar+", found "+temp+".");
+							}
+						}
+					}
+				}
+			}
+			fileScan.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fileOk;
+	}
 }
